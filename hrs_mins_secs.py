9766178c31518cadccdf21c534c7acaba3eb8df2
Raul Conover <rcon.mail@gmail.com>
2017-04-19 16:58:48 -0500

    Update hrs_mins_secs.py

--- hrs_mins_secs.py
+++ hrs_mins_secs.py
@@ -6,4 +6,4 @@ secs_still_remaining = total_secs %3600
 minutes = secs_still_remaining //60
 secs_finally_remaining =  secs_still_remaining %60
 
-print("Hrs=", hours, " mins=", minutes, " secs=", secs_finally_remaining)
+print("Hours = ", hours, " minutes = ", minutes, " seconds = ", secs_finally_remaining)
