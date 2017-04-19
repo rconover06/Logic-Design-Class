6826f527531a3432692533b967f1b4684c5c0cc9
Raul Conover <rcon.mail@gmail.com>
2017-04-19 17:00:52 -0500

    Update hrs_mins_secs.py

--- hrs_mins_secs.py
+++ hrs_mins_secs.py
@@ -1,6 +1,6 @@
 #Raul Conover
 
-total_secs = int(input("How many seconds, in total?"))
+total_secs = int(input("How many seconds, in total? "))
 hours = total_secs //3600
 secs_still_remaining = total_secs %3600
 minutes = secs_still_remaining //60
