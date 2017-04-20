acd2222abcfbc7592fc3e6bc7f68d39101fe32a0
Raul Conover <rcon.mail@gmail.com>
2017-04-19 20:34:05 -0500

    Created first turtle program
    ch 3 turtle program

---
+++ Turtles.py
@@ -0,0 +1,18 @@
+#Raul Conover
+
+# Allows the use of turtles
+import turtle
+#C reates a playground for turtlee
+wn = turtle.Screen()
+# Create a turtle, assign to alex
+alex = turtle.Turtle()
+
+# Tell alex to move forward 50 units
+alex.forward(50)
+# Tell alex to turn 90 degrees
+alex.left(90)
+# Complete the second side of a rectangle
+alex.forward(30)
+
+# Wait for user to close window
+wn.mainloop()
