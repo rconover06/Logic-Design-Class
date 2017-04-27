967edb84d50bdf302138721581667674c579ac3a
Raul Conover <rcon.mail@gmail.com>
2017-04-27 10:35:57 -0500

    commit

--- projects/python_project_2.py
+++ projects/python_project_2.py
@@ -2,20 +2,21 @@
 # Logic Design 1309
 # Create a program that is able to guess a number between 0 and 1000 in as few guesses as possible.
 
-import sys
-
 # Start Functions
 def guess_num(x,):
     """ Guess a number x and ask yes or no if it is it
     """
-    guess = str(input("Is your number", x, "? [Y/N]: "))
+    print(" Is your number", x, "?")
+    ans = str(input("[Y/N]:"))
     return ans
 
-def high_low(g,):
-    """ 
-    """
+#def high_low(g,):
+#    """
+#    """
 
 # End Functions
 
 x = 500
-ans = guess_num(x)
+ans = guess_num(x,)
+print(x, ans)
+
