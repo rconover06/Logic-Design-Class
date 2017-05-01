cb782982c0803fe720660e473cea11db2f3bb23c
Raul Conover <rcon.mail@gmail.com>
2017-05-01 16:46:38 -0500

    commit

--- projects/python_project_2.py
+++ projects/python_project_2.py
@@ -1,18 +1,18 @@
 # Raul Conover
 # Logic Design 1309
 # Create a program that is able to guess a number between 0 and 100
-# in as few guesses as possible.
+# in as few guesses as possible by working off higher or lower clues from user.
 
 # Start Functions
 def guess_num(guess,):
-    """ Ask if guess is correct, return ans
+    """ Ask if guess is correct, Ifreturn ans
     """
     print("Is your number", guess, "?")
     ans = str(input("[Y/N]:"))
+    print()
     if ans == "y" or ans == "yes":
-        print("I have guessed your number. It was", guess,".",
-              "It took", count,"attempts to guess it correctly")
-#        repeat = False
+        print("I have guessed your number. It was", guess,".")
+        print("It took", count,"attempts to guess it correctly")
     else:
         return ans
 
@@ -20,22 +20,23 @@ def high_low(guess,):
     """ Ask if guess is higher or lower, return hilo
     """
     print("Is your number higher or lower than", guess,"?")
-    hilo = str(input(": "))
+    hilo = str(input("Higher or Lower: "))
+    print()
     return hilo
 
 # End Functions
 
-# Set the initial range for the program.
-low = 0
-high = 100
+# Set the initial range (high and low), calculate guess, and create counter.
+low = -1
+high = 101
 guess = (high + low) // 2
-newguess = guess
-hilo = "n"
 count = 1
 
 # Setup the rules for the user
-print("Write down a number between", low,"to", high,".",
-      "I will try to guess it in as few guesses as possible.")
+print("Write down a number from", low + 1,"and", high - 1,".")
+print("If the guess is wrong tell me if your number is higer or lower.")
+print("I will try to guess it in as few guesses as possible.")
+print()
 
 repeat = True
 
@@ -49,21 +50,24 @@ while repeat:
         # Increase count by 1.
         if hilo == "h" or hilo == "higher":
             low = guess
-            newguess = (high + low) // 2
-            guess = newguess
+            guess = (high + low) // 2
             count = count + 1
         # else/If the number is lowe set high = guess then calculate newguess. Set newguess = guess.
         # Increase count by 1.
         elif hilo == "l" or hilo == "lower":
             high = guess
-            newguess = (low + high) // 2
-            guess = newguess
+            guess = (low + high) // 2
             count = count + 1
     # If the guess is correct end the loop.
     else:
         repeat = False
 
-# Debuging print to see what numbers are when they complete
-#print("ans:",ans,"hilo:", hilo, "low:", low, "high:", high, "guess:", guess, "newguess:", newguess)
+# Debuging prints to see what variables are when program completes.
+#print("ans:", ans)
+#print("hilo:", hilo)
+#print("count:", count)
+#print("low:", low)
+#print("high:", high)
+#print("guess:", guess)
 
 
