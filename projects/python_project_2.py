021ec3ee4a04ca88a265d4d2193b84a22b58847e
Raul Conover <rcon.mail@gmail.com>
2017-05-01 16:01:59 -0500

    commit
    Should work for now. Want to add more functions as I get better.

--- projects/python_project_2.py
+++ projects/python_project_2.py
@@ -3,21 +3,21 @@
 # Create a program that is able to guess a number between 0 and 100
 # in as few guesses as possible.
 
-import sys
-
 # Start Functions
 def guess_num(guess,):
-    """ Ask if guess is correct, return y or n
+    """ Ask if guess is correct, return ans
     """
     print("Is your number", guess, "?")
     ans = str(input("[Y/N]:"))
     if ans == "y" or ans == "yes":
-        print("I have guessed your number. It was", guess,".")
+        print("I have guessed your number. It was", guess,".",
+              "It took", count,"attempts to guess it correctly")
+#        repeat = False
     else:
         return ans
 
 def high_low(guess,):
-    """ Ask if number x is higher or lower
+    """ Ask if guess is higher or lower, return hilo
     """
     print("Is your number higher or lower than", guess,"?")
     hilo = str(input(": "))
@@ -25,41 +25,45 @@ def high_low(guess,):
 
 # End Functions
 
-
-
 # Set the initial range for the program.
 low = 0
 high = 100
 guess = (high + low) // 2
-
+newguess = guess
+hilo = "n"
+count = 1
 
 # Setup the rules for the user
-print("Write down a number from ", low," to ", high,".",
+print("Write down a number between", low,"to", high,".",
       "I will try to guess it in as few guesses as possible.")
 
 repeat = True
-ans = "y"
 
+# While repat is True.  Ask the user if guess is thir number.
 while repeat:
-    if ans == "y": #or ans == "yes":
-        ans = guess_num(guess,)
-    else:
+    ans = guess_num(guess,)
+    # if the answer is no or n ask user if their number is higher or lower.
+    if ans == "n" or ans == "no":
         hilo = high_low(guess,)
-        if hilo == "h":
+        # If the number is higher set low = guess then calculate newguess. Set newguess = guess.
+        # Increase count by 1.
+        if hilo == "h" or hilo == "higher":
             low = guess
-            newguess = ((high // 2) + high) // 2
+            newguess = (high + low) // 2
             guess = newguess
-            ans = guess_num(guess,)
-            newguess = ((high // 2) + high) // 2
-            guess =  newguess
-            ans = guess_num(guess,)
-        else:
+            count = count + 1
+        # else/If the number is lowe set high = guess then calculate newguess. Set newguess = guess.
+        # Increase count by 1.
+        elif hilo == "l" or hilo == "lower":
             high = guess
             newguess = (low + high) // 2
             guess = newguess
-            ans = guess_num(guess,)
+            count = count + 1
+    # If the guess is correct end the loop.
+    else:
         repeat = False
 
+# Debuging print to see what numbers are when they complete
 #print("ans:",ans,"hilo:", hilo, "low:", low, "high:", high, "guess:", guess, "newguess:", newguess)
 
 
